4e29c8c84921a604365b6a92f42e45363b86be25
jazzycrazzy <jagan.space@gmail.com>
2015-03-26 18:14:47 +0530

    Update FetchProductDetails.java

--- src/smb/inv/service/FetchProductDetails.java
+++ src/smb/inv/service/FetchProductDetails.java
@@ -1,1 +1,39 @@
+package smb.inv.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class FetchProductDetails {
+	
+	@Autowired
+	private DataSource dataSource;
+	Connection conn = null;
+	
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	public Product fetchProductDetails(String product, ProductBy productBY) throws SQLException{
+		
+		Product productDet = new Product();
+		
+		conn = dataSource.getConnection();
+		String query = "Select * from ";
+		if(productBY == ProductBy.ID)
+			query = query+"product";
+		else
+			query = query+"productInfo";
+		
+		return productDet;
+		
+	}
+
+}
