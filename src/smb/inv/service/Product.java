1d6e9f840066f0a2867f2553ffa69d3f97c9f5e7
jazzycrazzy <jagan.space@gmail.com>
2015-03-26 12:39:55 +0530

    Update Product.java

--- src/smb/inv/service/Product.java
+++ src/smb/inv/service/Product.java
@@ -1,1 +1,68 @@
+package smb.inv.service;
 
+import java.util.Map;
+import java.util.Date;
+
+public class Product {
+
+	private long productId;
+	private String productName;
+	private String modelNo;
+	private Map<String, String> specs;
+	private String descFileLoc;
+	private Date inflowDate;
+	private Date outflowDate;
+	
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getModelNo() {
+		return modelNo;
+	}
+	public void setModelNo(String modelNo) {
+		this.modelNo = modelNo;
+	}
+	public Map<String, String> getSpecs() {
+		return specs;
+	}
+	public void setSpecs(Map<String, String> specs) {
+		this.specs = specs;
+	}
+	public String getDescFileLoc() {
+		return descFileLoc;
+	}
+	public void setDescFileLoc(String descFileLoc) {
+		this.descFileLoc = descFileLoc;
+	}
+	public Date getInflowDate() {
+		return inflowDate;
+	}
+	public void setInflowDate(Date inflowDate) {
+		this.inflowDate = inflowDate;
+	}
+	public Date getOutflowDate() {
+		return outflowDate;
+	}
+	public void setOutflowDate(Date outflowDate) {
+		this.outflowDate = outflowDate;
+	}
+	
+	@Override
+	public String toString() {
+		String outflowDt = getOutflowDate()==null?"Not Sold":getOutflowDate().toString();
+		// TODO Auto-generated method stub
+		return "ProductId: "+getProductId()+", Product Name: "+getProductName()+", "+
+				"Model Number: "+getModelNo()+", Inflow Date: "+getInflowDate()+
+				", Outflow Date: "+ outflowDt;
+	}
+	
+}
